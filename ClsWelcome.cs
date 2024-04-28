4853de815bf5678150394c5f0c6bd88a51a4c861
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-04-28 11:40:51 +0630

    finished

---
+++ ClsWelcome.cs
@@ -0,0 +1,9 @@
+namespace ConsoleProgram
+{
+    class ClsWelcome {
+    private static void Main(string[] args) {
+        Console.WriteLine("Hello Everyone! My name is kaung myat han..");
+        Console.ReadLine();
+    }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleProgram
+{
+    static class Program {
+    //     private static void Main(string[] args) {
+    //     Console.WriteLine("Hola amigos.");
+    //     Console.ReadLine();
+    // }
+}
+}
