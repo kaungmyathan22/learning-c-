f777ed7311cc393ae348305d11c2e892a79f0bc6
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-05 12:23:42 +0630

    feat(): ass

---
+++ ch3ass2.cs
@@ -0,0 +1,10 @@
+class Ch3Ass2 {
+    static void Main(string[] args) {
+        int ticket = 0;
+        int speed = 50;
+        if(speed > 55) {
+            ticket = 1;
+        }
+        Console.WriteLine("Ticket = "+ticket);
+    }
+}
---
+++ ch3ass3.cs
@@ -0,0 +1,11 @@
+class Ch3Ass3 {
+    static void Main(string[] args) {
+        char ch ='y';
+        switch (ch)
+        {
+            case 'y': Console.WriteLine("Yes"); break;
+            case 'n': Console.WriteLine("Yes"); break;
+            default: Console.WriteLine("Unknown"); break;
+        }
+    }
+}
