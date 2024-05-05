cf071c79224b533ac21bd07bdc0bc866ed083e8c
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-05 10:39:58 +0630

    feat() .

---
+++ clscondition.cs
@@ -0,0 +1,23 @@
+class Condition {
+    static void Main(string[] args) {
+        int mark = 80;
+        if(mark < 40) {
+            Console.WriteLine("You failed the exam.");
+        }else if(mark >= 70) {
+            Console.WriteLine("You earned distinction.");
+        }else if(mark >= 60) {
+            Console.WriteLine("You earned merit.");
+        } else {
+            Console.WriteLine("You passed the exam.");
+        }
+        // if (mark == 50)
+        // {
+        //     Console.WriteLine("Mark is 50");
+        // } else if(mark > 50) {
+        //     Console.WriteLine("Mark is over 50");
+        // } else {
+        //     Console.WriteLine("Mark is not 50");
+        // }
+        Console.Read();
+    }
+}
