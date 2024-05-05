6f98b1d95278d5a9b8af2051c6c470549fa62862
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-05 12:40:44 +0630

    ,

--- loop.cs
+++ loop.cs
@@ -1,8 +1,12 @@
 class Loop {
     static void Main(string[] args) {
-      int i;
-      for(i =1; i <= 5; i++) {
-        Console.WriteLine(i);
-      }
+    //   int i;
+    //   for(i =1; i <= 5; i++) {
+    //     Console.WriteLine(i);
+    //   }
+    int j;
+    for(j=0; j < 5; j+=2) {
+        Console.WriteLine(j);
+    }
     }
 }
