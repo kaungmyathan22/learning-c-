9f4fdf1a791db5facaac4d6719d66f11910483ce
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-05 11:08:53 +0630

    gender

---
+++ clsgender.cs
@@ -0,0 +1,12 @@
+class CheckGender {
+    static void Main(string[] Args) {
+        char gender ='F';
+        if(gender == 'F') {
+            Console.WriteLine("Female");
+        } else if(gender == 'M') {
+            Console.WriteLine("Male");
+        } else {
+            Console.WriteLine("Other");
+        }
+    }
+}
