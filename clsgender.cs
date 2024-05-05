02cb6d6b606e769d4a04bbff0b6c80e1c101d8ca
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-05 11:31:07 +0630

    switch()

--- clsgender.cs
+++ clsgender.cs
@@ -1,12 +1,31 @@
 class CheckGender {
     static void Main(string[] Args) {
         char gender ='F';
-        if(gender == 'F') {
-            Console.WriteLine("Female");
-        } else if(gender == 'M') {
+        // if(gender == 'F' || gender == 'f') {
+        //     Console.WriteLine("Female");
+        // } else if(gender == 'M' || gender == 'm') {
+        //     Console.WriteLine("Male");
+        // } else {
+        //     Console.WriteLine("Other");
+        // }
+        switch (gender)
+        {
+            case  'M':
             Console.WriteLine("Male");
-        } else {
+            break;
+            case  'm':
+            Console.WriteLine("Male");
+            break;
+            case  'F':
+            Console.WriteLine("Female");
+            break;
+            case  'f':
+            Console.WriteLine("Female");
+            break;
+            default:
             Console.WriteLine("Other");
+            break;
+            // default:
         }
     }
 }
