073ebc045a2321460992c88817005e043af8c153
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-05-11 11:09:28 +0630

    chore(): comment out codes

--- loop.cs
+++ loop.cs
@@ -1,16 +1,16 @@
 class Loop {
-    static void Main(string[] args) {
-    //   int i;
-    //   for(i =1; i <= 5; i++) {
-    //     Console.WriteLine(i);
-    //   }
+    // static void Main(string[] args) {
+    // //   int i;
+    // //   for(i =1; i <= 5; i++) {
+    // //     Console.WriteLine(i);
+    // //   }
+    // // int j;
+    // // for(j=0; j < 5; j+=2) {
+    // //     Console.WriteLine(j);
+    // // }
     // int j;
-    // for(j=0; j < 5; j+=2) {
-    //     Console.WriteLine(j);
+    // for(j=0; j < 10; j+=2) {
+    //     Console.WriteLine(j + " * 2 = "+j * 2);
     // }
-    int j;
-    for(j=0; j < 10; j+=2) {
-        Console.WriteLine(j + " * 2 = "+j * 2);
-    }
-    }
+    // }
 }
